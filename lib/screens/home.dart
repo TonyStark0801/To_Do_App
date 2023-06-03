4942f2df88414c65f9a3062745f9df184cbfb268
Shubham Mishra <mishrashubham2001@gmail.com>
2023-06-04 00:47:20 +0530

    Skeleton Created

---
+++ lib/screens/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState()=> _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        centerTitle:  true,
+        title:  const Text("My ToDo App"),
+        elevation: 0,
+      ),
+      body: ListView(
+        children: [
+          ToDoList[],
+        ],
+      ),
+    );
+  }
+}
